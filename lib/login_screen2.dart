383d6746fab9b50e3ac611213848f50aa8a00bdd
guilospanck <guilherme.rp2014@gmail.com>
2020-10-28 14:02:27 -0300

    Initial commit

---
+++ lib/login_screen2.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen2 extends StatelessWidget {
+  final Color backgroundColor1;
+  final Color backgroundColor2;
+  final Color highlightColor;
+  final Color foregroundColor;
+  final AssetImage logo;
+  final BuildContext context;
+
+  void logIntoApp() {
+    Navigator.of(context)
+        .pushNamed('/welcome', arguments: {'name': "Jaian Sousa"});
+  }
+
+  LoginScreen2(
+      {Key k,
+      this.backgroundColor1,
+      this.backgroundColor2,
+      this.highlightColor,
+      this.foregroundColor,
+      this.context,
+      this.logo});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: new BoxDecoration(
+        gradient: new LinearGradient(
+          begin: Alignment.centerLeft,
+          end: new Alignment(
+              1.0, 0.0), // 10% of the width, so there are ten blinds.
+          colors: [
+            this.backgroundColor1,
+            this.backgroundColor2
+          ], // whitish to gray
+          tileMode: TileMode.repeated, // repeats the gradient over the canvas
+        ),
+      ),
+      height: MediaQuery.of(context).size.height,
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(top: 150.0, bottom: 50.0),
+            child: Center(
+              child: new Column(
+                children: <Widget>[
+                  Container(
+                    height: 128.0,
+                    width: 128.0,
+                    child: new CircleAvatar(
+                      backgroundColor: Colors.transparent,
+                      foregroundColor: this.foregroundColor,
+                      radius: 100.0,
+                      child: new Text(
+                        "J",
+                        style: TextStyle(
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.w100,
+                        ),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: this.foregroundColor,
+                        width: 1.0,
+                      ),
+                      shape: BoxShape.circle,
+                      //image: DecorationImage(image: this.logo)
+                    ),
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: new Text(
+                      "Jaian Sousa",
+                      style: TextStyle(color: this.foregroundColor),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          new Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.only(left: 40.0, right: 40.0),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                    color: this.foregroundColor,
+                    width: 0.5,
+                    style: BorderStyle.solid),
+              ),
+            ),
+            padding: const EdgeInsets.only(left: 0.0, right: 10.0),
+            child: new Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                new Padding(
+                  padding:
+                      EdgeInsets.only(top: 10.0, bottom: 10.0, right: 00.0),
+                  child: Icon(
+                    Icons.alternate_email,
+                    color: this.foregroundColor,
+                  ),
+                ),
+                new Expanded(
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'jaiansousa@flutter.com',
+                      hintStyle: TextStyle(color: this.foregroundColor),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          new Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.only(left: 40.0, right: 40.0, top: 10.0),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                    color: this.foregroundColor,
+                    width: 0.5,
+                    style: BorderStyle.solid),
+              ),
+            ),
+            padding: const EdgeInsets.only(left: 0.0, right: 10.0),
+            child: new Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                new Padding(
+                  padding:
+                      EdgeInsets.only(top: 10.0, bottom: 10.0, right: 00.0),
+                  child: Icon(
+                    Icons.lock_open,
+                    color: this.foregroundColor,
+                  ),
+                ),
+                new Expanded(
+                  child: TextField(
+                    obscureText: true,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: '*********',
+                      hintStyle: TextStyle(color: this.foregroundColor),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          new Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.only(left: 40.0, right: 40.0, top: 30.0),
+            alignment: Alignment.center,
+            child: new Row(
+              children: <Widget>[
+                new Expanded(
+                  child: new FlatButton(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20.0, horizontal: 20.0),
+                    color: this.highlightColor,
+                    onPressed: () => {logIntoApp()},
+                    child: Text(
+                      "Entrar",
+                      style: TextStyle(color: this.foregroundColor),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // new Container(
+          //   width: MediaQuery.of(context).size.width,
+          //   margin: const EdgeInsets.only(left: 40.0, right: 40.0, top: 10.0),
+          //   alignment: Alignment.center,
+          //   child: new Row(
+          //     children: <Widget>[
+          //       new Expanded(
+          //         child: new FlatButton(
+          //           padding: const EdgeInsets.symmetric(
+          //               vertical: 20.0, horizontal: 20.0),
+          //           color: Colors.transparent,
+          //           onPressed: () => {},
+          //           child: Text(
+          //             "Esqueceu sua senha?",
+          //             style: TextStyle(
+          //                 color: this.foregroundColor.withOpacity(0.5)),
+          //           ),
+          //         ),
+          //       ),
+          //     ],
+          //   ),
+          // ),
+          // new Expanded(
+          //   child: Divider(),
+          // ),
+          // new Container(
+          //   width: MediaQuery.of(context).size.width,
+          //   margin: const EdgeInsets.only(
+          //       left: 40.0, right: 40.0, top: 10.0, bottom: 20.0),
+          //   alignment: Alignment.center,
+          //   child: new Row(
+          //     children: <Widget>[
+          //       new Expanded(
+          //         child: new FlatButton(
+          //           padding: const EdgeInsets.symmetric(
+          //               vertical: 20.0, horizontal: 20.0),
+          //           color: Colors.transparent,
+          //           onPressed: () => {},
+          //           child: Text(
+          //             "Não tem uma conta? Crie uma.",
+          //             style: TextStyle(
+          //                 color: this.foregroundColor.withOpacity(0.5)),
+          //           ),
+          //         ),
+          //       ),
+          //     ],
+          //   ),
+          // ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,495 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import './login_screen2.dart';
+
+void main() => runApp(WorkApp());
+
+class WorkApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // remove debug banner
+      title: 'Work App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      routes: {
+        '/': (context) => LoginScreen(title: 'Que dia eu trabalho?'),
+        '/welcome': (context) => WelcomeScreen(title: 'Que dia eu trabalho?'),
+        '/result': (context) => ResultScreen(title: 'Que dia eu trabalho?'),
+      },
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: LoginForm(
+          title: title,
+        ),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  LoginForm({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: LoginScreen2(
+          backgroundColor1: Color(0xFF444152),
+          backgroundColor2: Color(0xFF6f6c7d),
+          highlightColor: Color(0xfff65aa3),
+          foregroundColor: Colors.white,
+          context: context),
+    );
+  }
+}
+
+class ResultScreen extends StatelessWidget {
+  ResultScreen({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: ResultForm(
+          title: title,
+        ),
+      ),
+    );
+  }
+}
+
+class ResultForm extends StatefulWidget {
+  ResultForm({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _ResultFormState createState() => _ResultFormState();
+}
+
+class _ResultFormState extends State<ResultForm> {
+  @override
+  Widget build(BuildContext context) {
+    // get Args from route
+    Map args = ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: args != null
+            ? ListView(children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(8.0),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8.0))),
+                    child: InkWell(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          ClipRRect(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(8.0),
+                              topRight: Radius.circular(8.0),
+                            ),
+                            child: args['willWork']
+                                ? (MediaQuery.of(context).size.width >= 768.0
+                                    ? Image.asset('img/trabalhar.jpeg',
+                                        height: 600, fit: BoxFit.fill)
+                                    : Image.asset('img/trabalhar.jpeg',
+                                        height: 400, fit: BoxFit.fill))
+                                : (MediaQuery.of(context).size.width >= 768.0
+                                    ? Image.asset('img/folga.jpeg',
+                                        height: 600, fit: BoxFit.fill)
+                                    : Image.asset('img/folga.jpeg',
+                                        height: 400, fit: BoxFit.fill)),
+                          ),
+                          ListTile(
+                            title: Text(args['result']),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ])
+            : new Container());
+  }
+}
+
+// ------------------- Welcome Screen --------------------
+class WelcomeScreen extends StatelessWidget {
+  WelcomeScreen({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: WelcomeForm(
+          title: title,
+        ),
+      ),
+    );
+  }
+}
+
+class WelcomeForm extends StatefulWidget {
+  WelcomeForm({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _WelcomeFormState createState() => _WelcomeFormState();
+}
+
+class _WelcomeFormState extends State<WelcomeForm> {
+  final _name = TextEditingController();
+  final _dateEditing = TextEditingController();
+  // Note: This is a GlobalKey<FormState>, not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  List<String> _options = ["Sim", "Não"];
+  String _selectedOption;
+  DateTime whatDayHeWants = DateTime.now();
+  bool visibilityOfDateText = false;
+
+  String _transformDateTimeIntoDate(DateTime date) {
+    var day = date.day;
+    var month = date.month;
+    var year = date.year;
+
+    var total = day.toString() + "/" + month.toString() + "/" + year.toString();
+    return total;
+  }
+
+  void _setDate(DateTime date) {
+    setState(() {
+      _dateEditing.text = _transformDateTimeIntoDate(date);
+      whatDayHeWants = date;
+    });
+  }
+
+  void _setVisibilityOfDateTextField(bool visibility) {
+    setState(() {
+      visibilityOfDateText = visibility;
+    });
+  }
+
+  String _verifyWhichTeamIsByDifference(int difference) {
+    var result = "";
+    if (difference % 2 == 0) {
+      result = "A";
+    } else {
+      result = "B";
+    }
+    return result;
+  }
+
+  int _getDifferenceOfDatesInDays(DateTime aboveDate) {
+    var beginningOfTheYear =
+        DateTime(aboveDate.year - 1, DateTime.december, 31);
+    var difference = aboveDate.difference(beginningOfTheYear).inDays;
+
+    return difference;
+  }
+
+  String _verifyWhichTeamIsHe(DateTime today, String isHeWorkingToday) {
+    var difference = _getDifferenceOfDatesInDays(today);
+    var teamToday = _verifyWhichTeamIsByDifference(difference); // A or B
+
+    var whatTeamIsHe = "null";
+    if (isHeWorkingToday == "Sim") {
+      whatTeamIsHe = teamToday;
+    } else {
+      whatTeamIsHe = teamToday == 'A' ? 'B' : 'A';
+    }
+
+    return whatTeamIsHe;
+  }
+
+  String _formatResponseToShowInResultScreen(
+      DateTime dayHeWantsToKnow, DateTime today, String workingOrNot) {
+    var response = "";
+    if (dayHeWantsToKnow.isAfter(today)) {
+      response =
+          _name.text + ", você " + workingOrNot + "irá trabalhar nesse dia.";
+    } else if (dayHeWantsToKnow.isAtSameMomentAs(today)) {
+      response =
+          _name.text + ", você " + workingOrNot + "está trabalhando hoje.";
+    } else if (dayHeWantsToKnow.isBefore(today)) {
+      response = _name.text + ", você " + workingOrNot + "trabalhou nesse dia.";
+    }
+
+    return response;
+  }
+
+  void _verifyIfHeIsGoingToWork() {
+    var today = DateTime.now();
+    var dayHeWantsToKnow = whatDayHeWants;
+    var isHeWorkingToday = _selectedOption;
+
+    var whatTeamIsHe = _verifyWhichTeamIsHe(today, isHeWorkingToday);
+
+    var difference = _getDifferenceOfDatesInDays(dayHeWantsToKnow);
+    var teamOfTheDay = _verifyWhichTeamIsByDifference(difference);
+
+    var response = "";
+    bool yesOrNo = false;
+    if (teamOfTheDay == whatTeamIsHe) {
+      response =
+          _formatResponseToShowInResultScreen(dayHeWantsToKnow, today, "");
+      yesOrNo = true;
+    } else {
+      response =
+          _formatResponseToShowInResultScreen(dayHeWantsToKnow, today, "não ");
+      yesOrNo = false;
+    }
+    _showResultScreen(response, yesOrNo);
+  }
+
+  void _showResultScreen(String result, bool willWork) {
+    Navigator.of(context).pushNamed('/result',
+        arguments: {'result': result, "willWork": willWork});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // get Args from route
+    Map args = ModalRoute.of(context).settings.arguments;
+
+    var bigScreen = Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Padding(
+          padding: EdgeInsets.fromLTRB(100.0, 20.0, 100.0, 20.0),
+          child: TextFormField(
+              // controller: _name,
+              initialValue: args['name'].toString(),
+              decoration: const InputDecoration(
+                icon: Icon(Icons.person),
+                hintText: 'Qual seu nome?',
+                labelText: 'Nome',
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Informe seu nome!';
+                }
+                return null;
+              }),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(100.0, 20.0, 100.0, 20.0),
+          child: DropdownButtonFormField(
+            decoration: const InputDecoration(
+              icon: Icon(Icons.work),
+              hintText: 'Você está trabalhando hoje?',
+              labelText: 'Trabalhando hoje? *',
+            ),
+            value: _selectedOption,
+            validator: (String value) {
+              if (value?.isEmpty ?? true) {
+                return 'Informe se está ou não trabalhando hoje!';
+              }
+              return null;
+            },
+            items: _options
+                .map((option) => DropdownMenuItem(
+                      child: new Text(option),
+                      value: option,
+                    ))
+                .toList(),
+            onChanged: (newValue) => setState(() => _selectedOption = newValue),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Expanded(
+              child: IconButton(
+                icon: Icon(Icons.date_range),
+                tooltip: "Selecione uma data",
+                onPressed: () {
+                  DatePicker.showDatePicker(context,
+                      showTitleActions: true,
+                      minTime: DateTime(2018, 1, 1),
+                      maxTime: DateTime(2118, 1, 1), onChanged: (date) {
+                    _setDate(date);
+                    _setVisibilityOfDateTextField(true);
+                  }, onConfirm: (date) {
+                    _setDate(date);
+                    _setVisibilityOfDateTextField(true);
+                  }, currentTime: DateTime.now(), locale: LocaleType.pt);
+                },
+              ),
+            ),
+            visibilityOfDateText
+                ? Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(100.0, 20.0, 100.0, 20.0),
+                      child: TextFormField(
+                          controller: _dateEditing,
+                          enabled: true,
+                          decoration: const InputDecoration(
+                            icon: Icon(Icons.date_range),
+                            hintText: 'Formato: xx/xx/xxxx',
+                            labelText: 'Data que deseja saber *',
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Selecione a data no botão acima ou informe com xx/xx/xxxx';
+                            }
+                            return null;
+                          }),
+                    ),
+                  )
+                : new Container(),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          child: RaisedButton(
+            onPressed: () {
+              if (_formKey.currentState.validate()) {
+                // Scaffold.of(context).showSnackBar(SnackBar(content: Text('Processando dados...')));
+                _verifyIfHeIsGoingToWork();
+              }
+            },
+            child: Text('Verificar'),
+          ),
+        ),
+      ],
+    );
+
+    var smallScreen = Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Padding(
+          padding: EdgeInsets.fromLTRB(100.0, 20.0, 100.0, 20.0),
+          child: TextFormField(
+              controller: _name,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.person),
+                hintText: 'Qual seu nome?',
+                labelText: 'Nome *',
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Informe seu nome!';
+                }
+                return null;
+              }),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(100.0, 20.0, 100.0, 20.0),
+          child: DropdownButtonFormField(
+            decoration: const InputDecoration(
+              icon: Icon(Icons.work),
+              hintText: 'Você está trabalhando hoje?',
+              labelText: 'Trabalhando hoje? *',
+            ),
+            value: _selectedOption,
+            validator: (String value) {
+              if (value?.isEmpty ?? true) {
+                return 'Informe se está ou não trabalhando hoje!';
+              }
+              return null;
+            },
+            items: _options
+                .map((option) => DropdownMenuItem(
+                      child: new Text(option),
+                      value: option,
+                    ))
+                .toList(),
+            onChanged: (newValue) => setState(() => _selectedOption = newValue),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(100.0, 20.0, 100.0, 20.0),
+          child: IconButton(
+            icon: Icon(Icons.date_range),
+            tooltip: "Selecione uma data",
+            onPressed: () {
+              DatePicker.showDatePicker(context,
+                  showTitleActions: true,
+                  minTime: DateTime(2018, 1, 1),
+                  maxTime: DateTime(2118, 1, 1), onChanged: (date) {
+                _setDate(date);
+                _setVisibilityOfDateTextField(true);
+              }, onConfirm: (date) {
+                _setDate(date);
+                _setVisibilityOfDateTextField(true);
+              }, currentTime: DateTime.now(), locale: LocaleType.pt);
+            },
+          ),
+        ),
+        Padding(
+            padding: EdgeInsets.fromLTRB(100.0, 20.0, 100.0, 20.0),
+            child: visibilityOfDateText
+                ? Padding(
+                    padding: EdgeInsets.fromLTRB(5.0, 20.0, 5.0, 20.0),
+                    child: TextFormField(
+                        controller: _dateEditing,
+                        enabled: true,
+                        decoration: const InputDecoration(
+                          icon: Icon(Icons.date_range),
+                          hintText: 'Formato: xx/xx/xxxx',
+                          labelText: 'Data que deseja saber *',
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Selecione a data no botão acima ou informe com xx/xx/xxxx';
+                          }
+                          return null;
+                        }),
+                  )
+                : new Container()),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          child: RaisedButton(
+            onPressed: () {
+              if (_formKey.currentState.validate()) {
+                // Scaffold.of(context).showSnackBar(SnackBar(content: Text('Processando dados...')));
+                _verifyIfHeIsGoingToWork();
+              }
+            },
+            child: Text('Verificar'),
+          ),
+        ),
+      ],
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Form(
+          key: _formKey,
+          child: MediaQuery.of(context).size.width >= 768.0
+              ? bigScreen
+              : smallScreen),
+    );
+  }
+}
